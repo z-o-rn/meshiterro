bd98a7170dc72483f44b44f2673fb2e073309428
EC2 Default User <ec2-user@ip-172-31-92-92.ec2.internal>
2021-02-10 13:34:25 +0000

    initial commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9492590b2f2698d739550024ee42b8593ee913eb32787d243e1ec56afcb1d8579f9c8cee017a7306894c4dff1754abee89edcd1b69af8f44a773d83fdea07625'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  root 'post_images#index'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+end
